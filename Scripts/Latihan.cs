3bb14cbd5951e65fec0252be3d66e598718b9da7
Alya Rusyda Maharistya <100183012+AlyaRusyda@users.noreply.github.com>
2022-11-25 08:53:11 +0700

    Add files via upload

---
+++ Scripts/Latihan.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start(){
+        Debug.Log("Saya sedang Belajar C# Unity");
+    }
+}
